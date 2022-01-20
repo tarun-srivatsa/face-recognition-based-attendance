c224f13c8601aa33cdbf08e9bc5412bc8e797809
Tarun Srivatsa <tarun.srivatsa.17@gmail.com>
2022-01-20 19:51:43 +0530

    added studentreg form

--- faceapp/urls.py
+++ faceapp/urls.py
@@ -3,4 +3,7 @@ from faceapp import views
 
 urlpatterns = [
     path('',views.homeview,name='home'),
+    path('facescan/',views.facescan,name='facescan'),
+    path('studentreg/',views.student_register,name='studentreg'),
+    path('create-session/',views.create_session,name='createsession'),
 ]
