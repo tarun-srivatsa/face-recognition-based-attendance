9d14fad64f3359be8afef3b5eaee333b42687805
Tarun Srivatsa <tarun.srivatsa.17@gmail.com>
2022-01-22 03:38:13 +0530

    active-sessions

--- faceapp/urls.py
+++ faceapp/urls.py
@@ -3,9 +3,9 @@ from faceapp import views
 
 urlpatterns = [
     path('',views.homeview,name='home'),
-    path('facescan/',views.facescan,name='facescan'),
-    path('studentreg/',views.student_register,name='studentreg'),
+    path('register-scan/',views.register_scan,name='registerscan'),
+    path('studentreg/',views.student_register,name='studentregister'),
     path('create-session/',views.create_session,name='createsession'),
     path('attendance-scan/',views.attendance_scan,name='attendancescan'),
-    # path('active-sessions/',views.create_session,name='activesession'),
+    path('active-sessions/',views.active_sessions,name='activesessions'),
 ]
